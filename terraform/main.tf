6b3ea4046b21734d4588d3c266f23eae347ce58c
Krishna <gmkrishna097@gmail.com>
2025-12-04 18:00:44 +0000

    fixing depends on

--- terraform/main.tf
+++ terraform/main.tf
@@ -43,21 +43,3 @@ resource "aws_iam_openid_connect_provider" "eks" {
 
   depends_on = [module.eks]
 }
-
-# OIDC Provider - created after EKS cluster
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_name
-  depends_on = [module.eks]
-}
-
-data "tls_certificate" "eks" {
-  url = data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer
-}
-
-resource "aws_iam_openid_connect_provider" "eks" {
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
-  url             = data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer
-
-  depends_on = [module.eks]
-}

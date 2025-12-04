d1ca98191e8b6c118ddb2796bae245f0efc53795
Krishna <gmkrishna097@gmail.com>
2025-12-04 18:29:34 +0000

    fixing ami type

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -89,7 +89,7 @@ resource "aws_eks_node_group" "mlops-node-group" {
   }
 
   instance_types = ["t3.medium"]
-  ami_type       = "AL2_x86_64"
+  ami_type       = "AL2023_x86_64_STANDARD"
   disk_size      = 20
   capacity_type  = "SPOT"
 
